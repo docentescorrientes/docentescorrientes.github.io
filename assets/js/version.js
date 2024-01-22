275639a35208ba88c45b04777429d1e56a295acc
Docentes Autoconvocados Corrientes <145226806+docentescorrientes@users.noreply.github.com>
2024-01-22 07:00:52 -0300

    Delete assets directory

--- assets/js/version.js
+++
@@ -1,3 +0,0 @@
-//Inicio - Versión JS
-const version = "8.0", fecha = 'enero de 2024';
-document.getElementById('versionH6').insertAdjacentHTML("beforeend", version + '<br>' + fecha + '<br> @augusalterats');

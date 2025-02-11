d644b99a3edfbdc04d6c410b327284f1f81ea9d3
augusalterats <augusalterats@gmail.com>
2025-02-11 17:19:18 -0300

    commit 04

--- assets/js/datosInfoVersionSimulador.js
+++ assets/js/datosInfoVersionSimulador.js
@@ -1,5 +1,5 @@
 // Start Versión
-const version = "5.0", fecha = "Enero de 2025";
+const version = "5.0", fecha = "Febrero de 2025";
 document.getElementById("versionH6").insertAdjacentHTML("beforeend", `${version}<br>${fecha}<br>@augusalterats`);
 // End Versión
 

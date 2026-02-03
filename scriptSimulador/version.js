7d4087a99f31d20d037ec747060f9dc55d9aa5f5
Augusto Alterats <augusalterats@gmail.com>
2026-02-03 13:07:53 -0300

    commit 01

--- scriptSimulador/version.js
+++ scriptSimulador/version.js
@@ -1,4 +1,4 @@
 // Start Versión
-const version = "5.5", fecha = "Enero de 2026";
+const version = "5.6", fecha = "Enero de 2026";
 document.getElementById("versionH6").insertAdjacentHTML("beforeend", `${version}<br>${fecha}<br>@augusalterats`);
 // End Versión

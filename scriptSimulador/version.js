1b2188874a7f19975e2552adaeaee7f8a6f11d13
qubitfisica <qubitfisica@gmail.com>
2026-06-24 07:54:54 -0300

    commit 01

--- scriptSimulador/version.js
+++ scriptSimulador/version.js
@@ -1,4 +1,4 @@
 // Start Versión
-const version = "5.9", fecha = "Junio de 2026";
+const version = "6.0", fecha = "Junio de 2026";
 document.getElementById("versionH6").insertAdjacentHTML("beforeend", `${version}<br>${fecha}<br>@augusalterats`);
 // End Versión
